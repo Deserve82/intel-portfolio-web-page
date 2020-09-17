6915c818f25860f62192860ec8b268f6f48b29f2
Victor <inth8204@gmail.com>
2020-09-17 17:13:39 +0900

    [feature] create models
    created education, experience, project, programming skill, user model. except programming skill model, all other models have relation with user model. Because I put importance of easy extensibility first, I splited information of user by classes.

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class ProgrammingSkill(models.Model):
+    name = models.CharField(max_length=25, blank=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Education(models.Model):
+    school_type = (
+        ('HS', 'HIGH_SCHOOL'),
+        ('COL', 'COLLEAGE'),
+        ('UNIV', 'UNIVERSITY'),
+        ('GS', 'GRADUATE_SCHOOL')
+    )
+    type = models.CharField(max_length=6, choices=school_type)
+    name = models.CharField(max_length=20, blank=False)
+    major = models.CharField(max_length=20, blank=True)
+    start_date = models.DateField(blank=True)
+    graduate_date = models.DateField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name + ' 학력 사항'
+
+    @property
+    def get_duration_days(self):
+        return (self.graduate_date - self.start_date).days
+
+
+class Experience(models.Model):
+    level = (
+        ('IN', 'INTERN'),
+        ('JU', 'JUNIOR'),
+        ('SE', 'SENIOR'),
+        ('MA', 'MANAGER')
+    )
+    role = models.CharField(max_length=5, choices=level)
+    company_name = models.CharField(max_length=30, blank=True)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name + ' 경력 사항'
+
+    @property
+    def get_duration_days(self):
+        return (self.end_date - self.start_date).days
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=30, blank=True)
+    description = models.TextField(blank=True)
+    image = models.CharField(max_length=255, blank=False)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
+    used_skills = models.ManyToManyField(ProgrammingSkill)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name + ' 프로젝트'
+
+    @property
+    def get_duration_days(self):
+        return (self.end_date - self.start_date).days
--- portfolio/users/models.py
+++ portfolio/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    name = models.CharField(null=False, max_length=50)
+    email = models.EmailField(blank=True, max_length=125)
+    address = models.CharField(blank=True, max_length=125)
+    profile_image = models.CharField(max_length=255, blank=False)
+    phone_number = models.CharField(blank=True, max_length=25)
+    description = models.TextField(blank=True)
+
+    USERNAME_FIELD = 'name'
+    REQUIRED_FIELDS = []
