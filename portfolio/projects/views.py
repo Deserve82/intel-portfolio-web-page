47d0069ccd5c833dc0e7f777f48a1d2be8218ab5
Victor <inth8204@gmail.com>
2020-09-19 13:07:03 +0900

    [feature] add chart.js
    using chart.js, can make chart of programming skill portion

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,3 +1,5 @@
+import json
+from collections import Counter
 from django.shortcuts import render
 from users.models import User
 from projects.models import Project, Education, Experience
@@ -8,10 +10,26 @@ def main(request, username):
     projects = Project.objects.all().filter(user=user)
     educations = Education.objects.all().filter(user=user)
     experience = Experience.objects.all().filter(user=user)
+
+    skill_data = []
+    for project in projects:
+        skill_data.extend(project.used_skills.all())
+    skill_data = Counter(skill_data)
+    labels = []
+    counter_data = []
+    for key, val in skill_data.items():
+        labels.append(key.name)
+        counter_data.append(str(val))
+
+    labels = ",".join(labels)
+    counter_data = ",".join(counter_data)
+
     data = {
         'user': user,
         'projects': projects,
         'educations': educations,
-        'experience': experience
+        'experience': experience,
+        'labels': labels,
+        'counter_data': counter_data
     }
     return render(request, 'index.html', data)
