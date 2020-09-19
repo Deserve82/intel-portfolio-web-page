555dd573887a7b952a716df2c5d3beeadd092df1
deserve82 <inth8204@gmail.com>
2020-09-19 15:03:12 +0900

    [feature] add chart.js
    using project skill stacks and durations of project, made pie graph and bar graph

--- portfolio/projects/views.py
+++ portfolio/projects/views.py
@@ -1,19 +1,23 @@
 import json
 from collections import Counter
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from users.models import User
 from projects.models import Project, Education, Experience
 
 
 def main(request, username):
-    user = User.objects.get(name=username)
+    user = get_object_or_404(User, name=username)
     projects = Project.objects.all().filter(user=user)
     educations = Education.objects.all().filter(user=user)
     experience = Experience.objects.all().filter(user=user)
 
     skill_data = []
+    project_names = []
+    project_duration = []
     for project in projects:
         skill_data.extend(project.used_skills.all())
+        project_names.append(project.name)
+        project_duration.append(project.get_duration_days)
     skill_data = Counter(skill_data)
     labels = []
     counter_data = []
@@ -21,15 +25,19 @@ def main(request, username):
         labels.append(key.name)
         counter_data.append(str(val))
 
-    labels = ",".join(labels)
-    counter_data = ",".join(counter_data)
+    skill_labels = ",".join(labels)
+    skill_counter_data = ",".join(counter_data)
+    project_duration = ",".join(project_duration)
+    project_names = ",".join(project_names)
 
     data = {
         'user': user,
         'projects': projects,
         'educations': educations,
         'experience': experience,
-        'labels': labels,
-        'counter_data': counter_data
+        'skill_labels': skill_labels,
+        'skill_counter_data': skill_counter_data,
+        'project_duration': project_duration,
+        'project_names': project_names
     }
     return render(request, 'index.html', data)
